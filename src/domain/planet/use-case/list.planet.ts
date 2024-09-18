2b8ed8e205177148d31a7f26f194e79d7edd0b27
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-09-18 18:08:26 -0300

    refactor: implementando metodo either no use-case

--- src/domain/planet/use-case/list.planet.ts
+++ src/domain/planet/use-case/list.planet.ts
@@ -1,11 +1,16 @@
+import { Either, right } from "src/errors/either/either";
 import Planet from "../entity/planet.entity";
 import { PlanetRepository } from "../repository/planet.repository";
+
+type Response = Either<null, Planet[]>
 
 export class ListPlanetUseCase {
 
     constructor(private readonly planetRepository: PlanetRepository) { }
 
-    async execute(): Promise<Planet[]> {
-        return await this.planetRepository.getAll();
+    async execute(): Promise<Response> {
+        const listPlanets = await this.planetRepository.getAll();
+
+        return right(listPlanets);
     }
 }
