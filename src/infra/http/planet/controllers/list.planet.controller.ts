c23e83853d4acdf5ce5d1c4d5820eb14c8f427e0
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-09-18 20:16:13 -0300

    feat: Finalizando endpoints Create e Listar todos os planetas. Criacao de um PRESENTER para conversao das respostas

--- src/infra/http/planet/controllers/list.planet.controller.ts
+++ src/infra/http/planet/controllers/list.planet.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, HttpCode } from "@nestjs/common";
+import { Controller, Get, HttpCode, InternalServerErrorException, Res } from "@nestjs/common";
+import { Response } from "express";
 import { ListPlanetUseCase } from "src/domain/planet/use-case/list.planet";
+import { PlanetPresenter } from "src/infra/presenters/planet.presenter";
 
 @Controller("/planet")
-export class PlanetController {
+export class ListPlanetController {
 
     constructor(private readonly listPlanet: ListPlanetUseCase) { }
 
     @Get()
     @HttpCode(200)
-    async handler() {
-        return await this.listPlanet.execute();
+    async handler(@Res() response: Response) {
+        const result = await this.listPlanet.execute();
+
+        if (result.isLeft()) {
+            throw new InternalServerErrorException('Internal Server Error.');
+        }
+
+        const planets = result.value.map(planet => PlanetPresenter.toHTTP(planet));
+
+        return response.json(planets);
     }
 }
